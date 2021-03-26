30bd44416733db2b8489002388eeaa245a73ff35
Nate <7nater.nelson@gmail.com>
2021-03-26 11:51:58 -0600

    Set up basic product and order relationships

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+    has_many :order_items
+    has_many :orders, through: :order_items
+    has_many :product_materials
+    has_many :materials, through: :product_materials
+    
 end
--- app/models/product_material.rb
+++ app/models/product_material.rb
@@ -1,2 +1,4 @@
 class ProductMaterial < ApplicationRecord
+    belongs_to :product 
+    belongs_to :material, class_name: :product
 end
