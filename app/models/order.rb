aa6461093888155b172e6db68149a7e3e7db1c3c
Nate <7nater.nelson@gmail.com>
2021-03-27 16:29:22 -0600

    Form Partial for orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
     has_many :order_items
     has_many :products, through: :order_items
+
+
+    def customer_names
+        "Name " << rand(1..10).to_s
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_173937) do
+ActiveRecord::Schema.define(version: 2021_03_27_222819) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer "product_id"
