c971682c169b57e4c2855b402cfd58c57d44394b
Nate <7nater.nelson@gmail.com>
2021-03-27 09:24:36 -0600

    Establish Product Self-Referential Associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,13 @@
 class Product < ApplicationRecord
     has_many :order_items
     has_many :orders, through: :order_items
+
+    # A product has many materials
     has_many :product_materials
     has_many :materials, through: :product_materials
-    
+
+    # A product as a material can belong to several products
+    has_many :material_products, class_name: "ProductMaterial", foreign_key: :material_id
+    has_many :products, through: :material_products
+
 end
--- app/models/product_material.rb
+++ app/models/product_material.rb
@@ -1,4 +1,4 @@
 class ProductMaterial < ApplicationRecord
     belongs_to :product 
-    belongs_to :material, class_name: :product
+    belongs_to :material, class_name: "Product"
 end
