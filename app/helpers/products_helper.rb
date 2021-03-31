d927b64c945ab4b315693cdddf7aa6e49da59715
Nate <7nater.nelson@gmail.com>
2021-03-31 17:00:40 -0600

    product full dimension message

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,6 +1,19 @@
 module ProductsHelper
 
-    
+    def full_dimensions(product)
+        string = ""
+        %i[length width height].each do |d|
+            value = product.send(d).to_s
+            unless value.nil? || value == 0.to_s
+                if string.empty?
+                    string = value 
+                else
+                    string << " x #{value}"
+                end
+            end
+        end
+        "#{string} #{pluralize(2, product.dimension_unit_type)}" unless string.empty?
+    end
 
 
 end
