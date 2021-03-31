d87dc0a87be916d50d2992cb09ad00b7a5fcf8cd
Nate <7nater.nelson@gmail.com>
2021-03-31 16:41:13 -0600

    Product Index View

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,35 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: %i[new show edit update destroy ]
+
+    def index 
+        @products = Product.all
+    end
+
     def new
-        @product = Product.new
+    end
+
+    def create
+        @product = Product.new product_params
+        if @product.save 
+            redirect_to product_path(@product)
+        else
+            @errors = @product.errors.full_messages
+            render :new
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(
+            :name, :description, :color,
+            :height, :width, :length, 
+            :dimension_unit_type, :weight,
+            :weight_unit_type, :unit_type
+        )
+    end
+
+    def set_product
+        @product = params[:id] ? Product.find(params[:id]) : Product.new
     end
 end
