f6d744a43b589c0d3159a6b3e061a7f473cb2ed5
Nate <7nater.nelson@gmail.com>
2021-03-31 19:00:26 -0600

    ProductMaterial Table for Product Show view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,7 @@ class ProductsController < ApplicationController
     end
 
     def update 
+        binding.pry
         @product = Product.find_by params[:id]
         @product.update(product_params)
         if @product.save
